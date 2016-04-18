2d475a098e7b93a07661cfa1e99cae4578d1f56e
MattGraffunder <MattGraffunder@users.noreply.github.com>
2016-04-17 20:18:48 -0500

    First Two Lecture Notes and Problem set 1

---
+++ Problem Sets/ps0.py
@@ -0,0 +1,9 @@
+# Problem Set 0 
+# Name: Matt Graffunder
+# Collaborators: None 
+# Time Spent: 0:15
+
+birthday = raw_input("Enter Your Birthday: ")
+lastName = raw_input("Enter Your Last Name: ")
+
+print lastName  + " " + birthday
